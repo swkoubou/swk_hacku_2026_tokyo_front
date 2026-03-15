e46e1ed1f491052facf12e5e8f34c91d9c302c83
haga <vscoderyuu@gmail.com>
2026-03-15 11:46:40 +0900

    再修正(仮ホームとカレンダーの遷移を実装)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,5 @@ dependencies {
     // Retrofit (API通信用)
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.navigation:navigation-compose:2.8.5")
 }
