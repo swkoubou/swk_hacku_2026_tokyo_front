78f9ba711c804c044befe7c834f50f5385c0a2c2
haga <vscoderyuu@gmail.com>
2026-03-15 15:25:20 +0900

    予定管理におけるUSER_UUIDをUserSettingより参照

--- app/src/main/java/com/example/myapplication/UserSetting.kt
+++ app/src/main/java/com/example/myapplication/UserSetting.kt
@@ -6,6 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+object UserConfig {
+    const val USER_UUID = "3c7a9a24-9e34-4f65-bc1e-9a6e6c7d7f12"
+}
+
 class UserSetting : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
